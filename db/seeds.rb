2e032a4030234aaea373feaf14b9058df90459e0
Dyan Carra <dyancarra@gmail.com>
2020-03-30 21:27:44 -0300

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+records = [['1.1.1.1', ['lorem.com', 'ipsum.com', 'dolor.com', 'amet.com']],
+					 ['2.2.2.2', ['ipsum.com']],
+					 ['3.3.3.3', ['ipsum.com', 'dolor.com', 'amet.com']],
+					 ['4.4.4.4', ['ipsum.com', 'dolor.com', 'sit.com', 'amet.com']],
+					 ['5.5.5.5', ['dolor.com', 'sit.com']]]
+
+records.each do |record|
+	Dns.create(ip: record[0], hostnames: record[1])
+end
